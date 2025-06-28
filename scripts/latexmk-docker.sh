f775ca2ec84ea2a3565ecdeac85ad21482960972
Miguel Martins <miguelrjmartins9@gmail.com>
2025-06-27 20:10:08 -0400

    refactor: remove legacy files and reorganize project structure
    - Remove old devcontainer configuration (moved to config/docker/)
    - Remove legacy scripts (replaced by new CLI commands)
    - Remove old templates (moved to config/templates/)
    - Remove root-level Docker and configuration files (reorganized)
    
    BREAKING CHANGE: Legacy file locations and script names are no longer valid

--- scripts/latexmk-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# scripts/latexmk-docker.sh
-# Wrapper script para usar latexmk dentro do container Docker
-
-# Passa todos os argumentos para o latexmk dentro do container
-docker exec latex-env latexmk "$@"
--- templates/settings.json.tpl
+++
@@ -1,6 +0,0 @@
-{
-  "latex-workshop.latex.autoBuild.run": "onSave",
-  "latex-workshop.latex.outDir": "${workspaceFolder}/out",
-  "latex-workshop.view.pdf.viewer": "tab",
-  "files.autoSave": "afterDelay"
-}
